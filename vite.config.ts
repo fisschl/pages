8235796e31ee3b1cbf6104faeb187538fad5a128
WangZhenghai <fisschl@163.com>
2023-06-20 16:29:16 +0800

    feat: readme

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "gpu",
+  base: "/gpu/",
   plugins: [vue()],
   build: {
     sourcemap: true,
