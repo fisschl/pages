847d30587f3ebc281ad96b10d92cc5fb7d6d85d3
WangZhenghai <fisschl@163.com>
2023-09-12 10:18:07 +0800

    feat: table selection

--- pages/table.vue
+++ pages/table.vue
@@ -1,16 +1,139 @@
 <script setup lang="ts">
-const people = [
-  // 生成20个假数据
-  ...Array.from({ length: 20 }, (_, i) => ({
-    id: i + 9,
+import { useEventListener } from "@vueuse/core";
+import { throttle } from "lodash-es";
+
+const columns = ["id", "name", "title", "email", "role"];
+
+const rows: {
+  [key: string]: string | number;
+}[] = [
+  ...Array.from({ length: 200 }, (_, i) => ({
+    id: i + 1,
     name: "Lindsay Walton",
     title: "Front-end Developer",
     email: "xxxxxxx.com",
     role: "Member",
   })),
 ];
+
+const container = ref<HTMLElement>();
+const start = ref<HTMLElement>();
+const selector = ref<HTMLElement>();
+
+onMounted(() => {
+  const div = document.createElement("div");
+  div.className = "selector";
+  selector.value = div;
+});
+
+useEventListener(container, "mousedown", (e) => {
+  if (!container.value) return;
+  if (!selector.value) return;
+  const target = e.target;
+  if (!(target instanceof HTMLElement)) return;
+  const td = target.closest("td");
+  if (!td) return;
+  start.value = td;
+  if (container.value.contains(selector.value)) return;
+  container.value.appendChild(selector.value);
+});
+
+const setSelectorPosition = (...elements: HTMLElement[]) => {
+  if (!container.value) return;
+  if (!selector.value) return;
+  const rects = elements.map((ele) => ele.getBoundingClientRect());
+  const left = Math.min(...rects.map((rect) => rect.left));
+  const top = Math.min(...rects.map((rect) => rect.top));
+  const right = Math.max(...rects.map((rect) => rect.right));
+  const bottom = Math.max(...rects.map((rect) => rect.bottom));
+  const { scrollTop, scrollLeft } = container.value;
+  selector.value.style.left = `${left + scrollLeft}px`;
+  selector.value.style.top = `${top + scrollTop}px`;
+  selector.value.style.width = `${right - left}px`;
+  selector.value.style.height = `${bottom - top}px`;
+};
+
+/**
+ * 判断两个矩形是否重叠
+ */
+const overlap = (a: DOMRect, b: DOMRect) => {
+  return !(
+    a.right <= b.left ||
+    b.right <= a.left ||
+    a.bottom <= b.top ||
+    b.bottom <= a.top
+  );
+};
+
+const findContainsCell = () => {
+  if (!container.value) return;
+  if (!selector.value) return;
+  const sRect = selector.value.getBoundingClientRect();
+  return Array.from(container.value.querySelectorAll("td")).filter((cell) => {
+    const rect = cell.getBoundingClientRect();
+    return overlap(sRect, rect);
+  });
+};
+
+const clearTextRange = throttle(() => {
+  return getSelection()?.removeAllRanges();
+}, 100);
+
+useEventListener("mousemove", (e) => {
+  if (!start.value) return;
+  if (!container.value) return;
+  if (!selector.value) return;
+  const target = e.target;
+  if (!(target instanceof HTMLElement)) return;
+  const td = target.closest("td");
+  if (!td) return;
+  setSelectorPosition(start.value, td);
+  clearTextRange();
+});
+
+useEventListener("mouseup", () => {
+  if (!start.value) return;
+  start.value = undefined;
+  const cells = findContainsCell();
+  cells?.forEach((cell) => {
+    cell.classList.add("selected");
+  });
+});
 </script>
 
 <template>
-  <UTable class="m-4" :rows="people" />
+  <div
+    ref="container"
+    class="prose relative h-screen w-screen max-w-none overflow-auto px-10 py-6 dark:prose-invert"
+  >
+    <table ref="container" class="table-ele">
+      <tr>
+        <th v-for="column in columns" :key="column">
+          {{ column.toUpperCase() }}
+        </th>
+      </tr>
+      <tr v-for="row in rows" :key="row.id">
+        <td
+          v-for="column in columns"
+          :key="column"
+          :class="`cell-${column}-${row.id}`"
+        >
+          {{ row[column] }}
+        </td>
+      </tr>
+    </table>
+  </div>
 </template>
+
+<style>
+.prose .selector {
+  border: 1px solid #46a53adc;
+  background-color: #3fcb2d50;
+  position: absolute;
+  pointer-events: none;
+}
+
+.prose td.selected {
+  background-color: #2d99cb7e;
+}
+</style>
