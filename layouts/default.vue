f00793c3b75b13b796bdfe31a32dedc823a21b4f
WangZhenghai <fisschl@163.com>
2023-10-10 13:01:29 +0800

    feat: 多级导航

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,50 +6,56 @@ const handleLinkClick = () => {
 
 const links = reactive([
   {
-    label: "主页",
-    icon: "i-tabler-home",
-    to: "/",
-    click: handleLinkClick,
+    label: "工具",
+    children: [
+      {
+        label: "主页",
+        icon: "i-tabler-home",
+        to: "/",
+        click: handleLinkClick,
+      },
+      {
+        label: "Markdown",
+        icon: "i-tabler-markdown",
+        to: "/md",
+        click: handleLinkClick,
+      },
+      {
+        label: "代码格式化",
+        icon: "i-tabler-indent-increase",
+        to: "/format",
+        click: handleLinkClick,
+      },
+    ],
   },
   {
-    label: "Markdown",
-    icon: "i-tabler-markdown",
-    to: "/md",
-    click: handleLinkClick,
-  },
-  {
-    label: "代码格式化",
-    icon: "i-tabler-indent-increase",
-    to: "/format",
-    click: handleLinkClick,
-  },
-  {
-    label: "AI",
-    icon: "i-tabler-brand-openai",
-    to: "https://gpt.fisschl.world",
-    click: handleLinkClick,
-    target: "_blank",
-  },
-  {
-    label: "博客",
-    icon: "i-tabler-brand-blogger",
-    to: "https://memos.fisschl.world",
-    click: handleLinkClick,
-    target: "_blank",
-  },
-  {
-    label: "共享文件",
-    icon: "i-tabler-device-floppy",
-    to: "https://store.fisschl.world",
-    click: handleLinkClick,
-    target: "_blank",
-  },
-  {
-    label: "代码仓库",
-    icon: "i-tabler-brand-git",
-    to: "https://git.fisschl.world",
-    click: handleLinkClick,
-    target: "_blank",
+    label: "页面",
+    children: [
+      {
+        label: "AI",
+        icon: "i-tabler-brand-openai",
+        to: "https://gpt.fisschl.world",
+        target: "_blank",
+      },
+      {
+        label: "博客",
+        icon: "i-tabler-brand-blogger",
+        to: "https://memos.fisschl.world",
+        target: "_blank",
+      },
+      {
+        label: "共享文件",
+        icon: "i-tabler-device-floppy",
+        to: "https://store.fisschl.world",
+        target: "_blank",
+      },
+      {
+        label: "代码仓库",
+        icon: "i-tabler-brand-git",
+        to: "https://git.fisschl.world",
+        target: "_blank",
+      },
+    ],
   },
 ]);
 const lg = useMediaQuery(LG);
@@ -78,9 +84,21 @@ const handleChangeNavVisible = (value: boolean) => {
       <nav v-if="nav.isVerticalNavVisible" class="hidden lg:block">
         <UVerticalNavigation
           :links="links"
-          class="sticky top-14 w-48 py-4 pl-3"
-          :class="$style.nav"
-        />
+          class="sticky top-14 w-48 overflow-auto py-4 pl-3"
+          :class="$style['nav-height']"
+        >
+          <template #default="{ link }">
+            <div class="w-full text-left">
+              <div class="mb-2">
+                {{ link.label }}
+              </div>
+              <UVerticalNavigation
+                v-if="link.children"
+                :links="link.children"
+              />
+            </div>
+          </template>
+        </UVerticalNavigation>
       </nav>
       <USlideover
         v-model="nav.isDrawerNavVisible"
@@ -95,8 +113,7 @@ const handleChangeNavVisible = (value: boolean) => {
 </template>
 
 <style module>
-.nav {
-  height: calc(100vh - 4rem);
-  overflow: auto;
+.nav-height {
+  height: calc(100vh - 5rem);
 }
 </style>
