09321d8e6fed0c1d398094abcaa461d3b6590246
WangZhenghai <fisschl@163.com>
2023-10-17 09:01:02 +0800

    feat: 页脚

--- layouts/default.vue
+++ layouts/default.vue
@@ -29,6 +29,15 @@ whenever(
       </USlideover>
       <slot />
     </div>
+    <footer class="my-10 text-center text-sm text-gray-500">
+      <a
+        href="https://beian.miit.gov.cn/"
+        target="_blank"
+        class="hover:underline"
+      >
+        豫ICP备2023011860号-1
+      </a>
+    </footer>
   </div>
 </template>
 
--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,14 +1,5 @@
 <template>
-  <div class="flex-1">
-    <ContentDoc class="prose mx-5 my-4 max-w-none flex-1 dark:prose-invert" />
-    <a
-      href="https://beian.miit.gov.cn/"
-      target="_blank"
-      class="my-10 block text-center text-sm text-gray-500 hover:underline"
-    >
-      豫ICP备2023011860号-1
-    </a>
-  </div>
+  <ContentDoc class="prose mx-5 my-4 max-w-none flex-1 dark:prose-invert" />
 </template>
 
 <script setup lang="ts"></script>
