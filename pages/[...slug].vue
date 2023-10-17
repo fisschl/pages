9e7e2c2da42324e5fb6c9c5c81f0998bb721714c
WangZhenghai <fisschl@163.com>
2023-10-17 08:51:36 +0800

    feat: 备案号

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,3 +1,14 @@
 <template>
-  <ContentDoc class="prose mx-5 my-4 max-w-none flex-1 dark:prose-invert" />
+  <div class="flex-1">
+    <ContentDoc class="prose mx-5 my-4 max-w-none flex-1 dark:prose-invert" />
+    <a
+      href="https://beian.miit.gov.cn/"
+      target="_blank"
+      class="my-10 block text-center text-sm text-gray-500 hover:underline"
+    >
+      豫ICP备2023011860号-1
+    </a>
+  </div>
 </template>
+
+<script setup lang="ts"></script>
