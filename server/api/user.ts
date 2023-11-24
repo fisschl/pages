e30f082f1306ae2f3e74084c49960770d1249375
WangZhenghai <fisschl@163.com>
2023-11-24 13:03:27 +0800

    feat: user 接口不再写 cookie

--- server/api/user.ts
+++ server/api/user.ts
@@ -2,8 +2,5 @@ import { checkUser } from "../utils/user";
 
 export default defineEventHandler(async (event) => {
   const { token } = getQuery(event);
-  if (token && typeof token === "string") {
-    setCookie(event, "token", token);
-  }
-  return checkUser(event);
+  return checkUser(event, token);
 });
--- server/utils/user.ts
+++ server/utils/user.ts
@@ -1,20 +1,19 @@
 import type { User } from "@prisma/client";
 import type { H3Event, EventHandlerRequest } from "h3";
 
-export const getUser = async (
-  event: H3Event<EventHandlerRequest>,
-): Promise<User | undefined> => {
-  const token = getCookie(event, "token");
-  if (!token) return;
+export const getUser = async (token: unknown): Promise<User | undefined> => {
+  if (!token || typeof token !== "string") return;
   const str = await redis.get(token);
   if (!str) return;
   return JSON.parse(str);
 };
 
-export const checkUser: typeof getUser = async (
+export const checkUser = async (
   event: H3Event<EventHandlerRequest>,
+  token?: unknown,
 ): Promise<User> => {
-  const user = await getUser(event);
+  if (!token) token = getCookie(event, "token");
+  const user = await getUser(token);
   if (!user) throw createError({ status: 401 });
   return user;
 };
