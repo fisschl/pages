329708fb5420f3e7b1f24e1af920579940735a99
Fisschl <fisschl@163.com>
2023-11-28 21:10:12 +0800

    feat: table

--- pages/editor.vue
+++ pages/editor.vue
@@ -24,11 +24,14 @@ import Strike from "@tiptap/extension-strike";
 import Text from "@tiptap/extension-text";
 import Typography from "@tiptap/extension-typography";
 import { Editor, EditorContent } from "@tiptap/vue-3";
+import Link from "@tiptap/extension-link";
 import { debounce, pick } from "lodash-es";
 import { IndexeddbPersistence } from "y-indexeddb";
+import Table from "@tiptap/extension-table";
+import TableCell from "@tiptap/extension-table-cell";
+import TableHeader from "@tiptap/extension-table-header";
+import TableRow from "@tiptap/extension-table-row";
 import { Doc } from "yjs";
-import TaskItem from "@tiptap/extension-task-item";
-import TaskList from "@tiptap/extension-task-list";
 
 const user = await useMustLogin();
 const route = useRoute();
@@ -64,6 +67,9 @@ onMounted(() => {
     token: token.value,
     preserveConnection: false,
     document: ydoc,
+    onAuthenticationFailed: () => {
+      alert("连接失败，请刷新页面重试");
+    },
   });
   editor.value = new Editor({
     editorProps: {
@@ -97,13 +103,16 @@ onMounted(() => {
       Bold,
       Italic,
       OrderedList,
+      Link,
       HardBreak,
       Blockquote,
       CodeBlock,
       BulletList,
+      Table,
+      TableRow,
+      TableHeader,
+      TableCell,
       ListItem,
-      TaskList,
-      TaskItem,
     ],
   });
 });
@@ -184,133 +193,172 @@ const items = [
       @input="updateArticleName"
     />
   </div>
-  <div
-    v-if="editor"
-    class="sticky top-0 z-10 flex flex-wrap items-center gap-2 bg-zinc-50 px-4 py-2 dark:bg-zinc-900"
-  >
-    <UButton
-      size="xs"
-      icon="i-tabler-bold"
-      title="粗体"
-      :disabled="!editor.can().chain().focus().toggleBold().run()"
-      :color="editor.isActive('bold') ? 'black' : 'white'"
-      @click="editor.chain().focus().toggleBold().run()"
-    />
-    <UButton
-      size="xs"
-      icon="i-tabler-italic"
-      title="斜体"
-      :disabled="!editor.can().chain().focus().toggleItalic().run()"
-      :color="editor.isActive('italic') ? 'black' : 'white'"
-      @click="editor.chain().focus().toggleItalic().run()"
-    />
-    <UButton
-      size="xs"
-      icon="i-tabler-strikethrough"
-      title="删除线"
-      :disabled="!editor.can().chain().focus().toggleStrike().run()"
-      :color="editor.isActive('strike') ? 'black' : 'white'"
-      @click="editor.chain().focus().toggleStrike().run()"
-    />
-    <UButton
-      size="xs"
-      icon="i-tabler-code"
-      title="代码"
-      :disabled="!editor.can().chain().focus().toggleCode().run()"
-      :color="editor.isActive('code') ? 'black' : 'white'"
-      @click="editor.chain().focus().toggleCode().run()"
-    />
-    <UButton
-      size="xs"
-      icon="i-tabler-highlight"
-      title="高亮"
-      :disabled="!editor.can().chain().focus().toggleHighlight().run()"
-      :color="editor.isActive('highlight') ? 'black' : 'white'"
-      @click="editor.chain().focus().toggleHighlight().run()"
-    />
-    <UButton size="xs" icon="i-tabler-eraser" color="white" @click="clear" />
-    <UButton
-      icon="i-tabler-letter-p"
-      size="xs"
-      title="段落"
-      :color="editor.isActive('paragraph') ? 'black' : 'white'"
-      @click="editor.chain().focus().setParagraph().run()"
-    />
-    <UDropdown :items="items" :popper="{ placement: 'bottom-start' }">
+  <div v-if="editor" class="sticky top-0 z-10 bg-zinc-50 dark:bg-zinc-900">
+    <div class="flex flex-wrap items-center gap-2 px-4 py-2">
+      <UButton
+        v-if="editor.can().chain().focus().toggleBold().run()"
+        size="xs"
+        icon="i-tabler-bold"
+        title="粗体"
+        :color="editor.isActive('bold') ? 'black' : 'white'"
+        @click="editor.chain().focus().toggleBold().run()"
+      />
+      <UButton
+        v-if="editor.can().chain().focus().toggleItalic().run()"
+        size="xs"
+        icon="i-tabler-italic"
+        title="斜体"
+        :color="editor.isActive('italic') ? 'black' : 'white'"
+        @click="editor.chain().focus().toggleItalic().run()"
+      />
+      <UButton
+        v-if="editor.can().chain().focus().toggleStrike().run()"
+        size="xs"
+        icon="i-tabler-strikethrough"
+        title="删除线"
+        :color="editor.isActive('strike') ? 'black' : 'white'"
+        @click="editor.chain().focus().toggleStrike().run()"
+      />
+      <UButton
+        v-if="editor.can().chain().focus().toggleCode().run()"
+        size="xs"
+        icon="i-tabler-code"
+        title="代码"
+        :color="editor.isActive('code') ? 'black' : 'white'"
+        @click="editor.chain().focus().toggleCode().run()"
+      />
+      <UButton
+        v-if="editor.can().chain().focus().toggleHighlight().run()"
+        size="xs"
+        icon="i-tabler-highlight"
+        title="高亮"
+        :color="editor.isActive('highlight') ? 'black' : 'white'"
+        @click="editor.chain().focus().toggleHighlight().run()"
+      />
+      <UButton size="xs" icon="i-tabler-eraser" color="white" @click="clear" />
+      <UButton
+        icon="i-tabler-letter-p"
+        size="xs"
+        title="段落"
+        :color="editor.isActive('paragraph') ? 'black' : 'white'"
+        @click="editor.chain().focus().setParagraph().run()"
+      />
+      <UDropdown :items="items" mode="hover">
+        <UButton
+          size="xs"
+          icon="i-tabler-heading"
+          title="标题"
+          :color="editor.isActive('heading') ? 'black' : 'white'"
+        />
+      </UDropdown>
       <UButton
         size="xs"
-        icon="i-tabler-heading"
-        title="标题"
-        :color="editor.isActive('heading') ? 'black' : 'white'"
-      />
-    </UDropdown>
-    <UButton
-      size="xs"
-      icon="i-tabler-list"
-      title="列表"
-      :color="editor.isActive('bulletList') ? 'black' : 'white'"
-      @click="editor.chain().focus().toggleBulletList().run()"
-    />
-    <UButton
-      size="xs"
-      icon="i-tabler-list"
-      title="任务列表"
-      :color="editor.isActive('taskList') ? 'black' : 'white'"
-      @click="editor.chain().focus().toggleTaskList().run()"
-    />
-    <UButton
-      size="xs"
-      icon="i-tabler-list-numbers"
-      title="有序列表"
-      :color="editor.isActive('orderedList') ? 'black' : 'white'"
-      @click="editor.chain().focus().toggleOrderedList().run()"
-    />
-    <UButton
-      size="xs"
-      icon="i-tabler-source-code"
-      title="代码块"
-      :color="editor.isActive('codeBlock') ? 'black' : 'white'"
-      @click="editor.chain().focus().toggleCodeBlock().run()"
-    />
-    <UButton
-      size="xs"
-      icon="i-tabler-blockquote"
-      title="引用"
-      :color="editor.isActive('blockquote') ? 'black' : 'white'"
-      @click="editor.chain().focus().toggleBlockquote().run()"
-    />
-    <UButton
-      size="xs"
-      icon="i-tabler-spacing-vertical"
-      color="white"
-      title="分割线"
-      @click="editor.chain().focus().setHorizontalRule().run()"
-    />
-    <UButton
-      size="xs"
-      color="white"
-      title="强制换行"
-      icon="i-tabler-corner-down-left-double"
-      @click="editor.chain().focus().setHardBreak().run()"
-    />
-    <UButtonGroup size="xs">
-      <UButton
-        color="white"
-        icon="i-tabler-arrow-back-up"
-        title="撤销"
-        :disabled="!editor.can().chain().focus().undo().run()"
-        @click="editor.chain().focus().undo().run()"
+        icon="i-tabler-list"
+        title="列表"
+        :color="editor.isActive('bulletList') ? 'black' : 'white'"
+        @click="editor.chain().focus().toggleBulletList().run()"
       />
       <UButton
-        color="white"
-        icon="i-tabler-arrow-forward-up"
-        title="重做"
-        :disabled="!editor.can().chain().focus().redo().run()"
-        @click="editor.chain().focus().redo().run()"
+        size="xs"
+        icon="i-tabler-list-numbers"
+        title="有序列表"
+        :color="editor.isActive('orderedList') ? 'black' : 'white'"
+        @click="editor.chain().focus().toggleOrderedList().run()"
       />
-    </UButtonGroup>
+      <UButton
+        size="xs"
+        icon="i-tabler-source-code"
+        title="代码块"
+        :color="editor.isActive('codeBlock') ? 'black' : 'white'"
+        @click="editor.chain().focus().toggleCodeBlock().run()"
+      />
+      <UButton
+        size="xs"
+        icon="i-tabler-blockquote"
+        title="引用"
+        :color="editor.isActive('blockquote') ? 'black' : 'white'"
+        @click="editor.chain().focus().toggleBlockquote().run()"
+      />
+      <UButton
+        size="xs"
+        icon="i-tabler-spacing-vertical"
+        color="white"
+        title="分割线"
+        @click="editor.chain().focus().setHorizontalRule().run()"
+      />
+      <UButton
+        size="xs"
+        color="white"
+        title="强制换行"
+        icon="i-tabler-corner-down-left-double"
+        @click="editor.chain().focus().setHardBreak().run()"
+      />
+      <UButton
+        v-if="!editor.isActive('table')"
+        size="xs"
+        color="white"
+        icon="i-tabler-table"
+        title="表格"
+        @click="editor.chain().focus().insertTable({ rows: 5, cols: 3 }).run()"
+      />
+    </div>
+    <div v-if="editor.isActive('table')" class="flex flex-wrap gap-2 px-4 pb-2">
+      <UButton @click="editor.chain().focus().addColumnBefore().run()">
+        addColumnBefore
+      </UButton>
+      <UButton @click="editor.chain().focus().addColumnAfter().run()">
+        addColumnAfter
+      </UButton>
+      <UButton @click="editor.chain().focus().deleteColumn().run()">
+        deleteColumn
+      </UButton>
+      <UButton @click="editor.chain().focus().addRowBefore().run()">
+        addRowBefore
+      </UButton>
+      <UButton @click="editor.chain().focus().addRowAfter().run()">
+        addRowAfter
+      </UButton>
+      <UButton @click="editor.chain().focus().deleteRow().run()">
+        deleteRow
+      </UButton>
+      <UButton @click="editor.chain().focus().deleteTable().run()">
+        deleteTable
+      </UButton>
+      <UButton @click="editor.chain().focus().mergeCells().run()">
+        mergeCells
+      </UButton>
+      <UButton @click="editor.chain().focus().splitCell().run()">
+        splitCell
+      </UButton>
+      <UButton @click="editor.chain().focus().toggleHeaderColumn().run()">
+        toggleHeaderColumn
+      </UButton>
+      <UButton @click="editor.chain().focus().toggleHeaderRow().run()">
+        toggleHeaderRow
+      </UButton>
+      <UButton @click="editor.chain().focus().toggleHeaderCell().run()">
+        toggleHeaderCell
+      </UButton>
+      <UButton @click="editor.chain().focus().mergeOrSplit().run()">
+        mergeOrSplit
+      </UButton>
+      <UButton
+        @click="editor.chain().focus().setCellAttribute('colspan', 2).run()"
+      >
+        setCellAttribute
+      </UButton>
+      <UButton @click="editor.chain().focus().fixTables().run()">
+        fixTables
+      </UButton>
+      <UButton @click="editor.chain().focus().goToNextCell().run()">
+        goToNextCell
+      </UButton>
+      <UButton @click="editor.chain().focus().goToPreviousCell().run()">
+        goToPreviousCell
+      </UButton>
+    </div>
   </div>
-  <EditorContent :editor="editor" class="mx-4 mb-10 mt-3 flex-1" />
+  <EditorContent :editor="editor" class="mx-4 mt-3 flex-1 pb-10" />
 </template>
 
 <style module></style>
@@ -333,10 +381,38 @@ const items = [
   user-select: none;
 }
 
+.tiptap > * p {
+  margin: 0;
+}
+
 .tiptap p.is-editor-empty:first-child::before {
   content: attr(data-placeholder);
   float: left;
   color: #adb5bd8a;
   pointer-events: none;
+}
+
+.tiptap table th {
+  background-color: rgba(137, 141, 145, 0.168);
+}
+.tiptap table th,
+.tiptap table td {
+  position: relative;
+  border: 1px solid var(--tw-prose-td-borders);
+  padding: 0.4em 0.5em;
+}
+.tiptap table .selectedCell:after {
+  position: absolute;
+  content: "";
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  background: rgba(66, 140, 92, 0.097);
+  pointer-events: none;
+}
+
+.tiptap .ProseMirror-gapcursor::after {
+  border-top: 1px solid var(--tw-prose-body);
 }
 </style>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,9 @@
 import type { Config } from "tailwindcss";
 import typography from "@tailwindcss/typography";
+import forms from "@tailwindcss/forms";
 
 const config: Partial<Config> = {
-  plugins: [typography],
+  plugins: [typography, forms],
 };
 
 export default config;
