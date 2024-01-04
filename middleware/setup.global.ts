b7d0af35969a5702f9242d25c6b83c1e557eab5e
WangZhenghai <fisschl@163.com>
2024-01-04 10:52:52 +0800

    feat: 重定向

--- middleware/setup.global.ts
+++
@@ -1,3 +0,0 @@
-export default defineNuxtRouteMiddleware((to) => {
-  if (to.path === "/") return navigateTo("/main/home");
-});
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+await navigateTo("/main/home");
+</script>
+
+<template>
+  <main class="flex h-screen w-screen items-center justify-center">
+    <UIcon
+      name="i-tabler-loader"
+      class="animate-spin"
+      style="font-size: 1.2rem"
+    />
+  </main>
+</template>
