e9a67a7dd80b59f4a0a230bbd8cd582ab8f7a4b8
WangZhenghai <fisschl@163.com>
2024-01-23 10:22:09 +0800

    feat: 移除监控

--- server/api/sse.ts
+++ server/api/sse.ts
@@ -1,45 +1,12 @@
-import type { H3Event, EventHandlerRequest } from "h3";
+import type { EventHandlerRequest, H3Event } from "h3";
 import { checkUserSafe } from "../utils/password";
 import { subscriber } from "../utils/redis";
-import { cpus, totalmem, freemem } from "node:os";
 
 export const useSseKey = async (event: H3Event<EventHandlerRequest>) => {
   const user = await checkUserSafe(event);
   if (typeof user === "number") return `sse:guest`;
   return `sse:${user.id}`;
 };
-
-/**
- * user CPU 在用户模式下花费的毫秒数。
- * nice CPU 在良好模式下花费的毫秒数。
- * sys CPU 在系统模式下花费的毫秒数。
- * idle CPU 在空闲模式下花费的毫秒数。
- * irq CPU 在中断请求模式下花费的毫秒数。
- */
-export const cpuUsage = async () => {
-  const _list = cpus();
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  return cpus().map((item, index) => {
-    const _item = _list[index];
-    item.times.user -= _item.times.user;
-    item.times.nice -= _item.times.nice;
-    item.times.sys -= _item.times.sys;
-    item.times.idle -= _item.times.idle;
-    item.times.irq -= _item.times.irq;
-    return item;
-  });
-};
-
-const analysis = async () => {
-  const cpu = await cpuUsage();
-  const mem = {
-    total: totalmem(),
-    used: totalmem() - freemem(),
-  };
-  return { cpu, mem, time: new Date().toISOString() };
-};
-
-export type AnalysisData = Awaited<ReturnType<typeof analysis>>;
 
 export default defineEventHandler(async (event) => {
   if (!subscriber.isOpen) await subscriber.connect();
@@ -61,21 +28,12 @@ export default defineEventHandler(async (event) => {
 
   await subscriber.subscribe(sseKey, sendMessage);
 
-  const analysisTimer = setInterval(async () => {
-    const str = JSON.stringify({
-      type: "analysis",
-      data: await analysis(),
-    });
-    sendMessage(str);
-  }, 1000);
-
   const { req, res } = event.node;
 
   const cleanup = async () => {
     if (!writer.closed) await writer.close();
     if (!res.closed) res.end();
     await subscriber.unsubscribe(sseKey, sendMessage);
-    clearInterval(analysisTimer);
   };
 
   req.on("close", cleanup);
