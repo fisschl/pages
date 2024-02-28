40c518e4a60e21aeaa719be88c10f8802631e12f
WangZhenghai <fisschl@163.com>
2024-02-28 19:07:08 +0800

    feat: 背景音乐

--- components/main/HeaderBar.vue
+++ components/main/HeaderBar.vue
@@ -1,5 +1,67 @@
 <script setup lang="ts">
+import type { DropdownItem } from "#ui/types";
+import { Howl } from "howler";
+import { first, once, sample } from "lodash-es";
+
 const navbar = defineModel<boolean>("navbar");
+
+const sound = shallowRef<Howl>();
+
+const musics = [
+  {
+    label: "赴大荒",
+    src: "https://cdn.fisschl.world/static/赴大荒.webm",
+  },
+];
+
+const currentMusic = ref(first(musics)!);
+
+const stopMusic = () => {
+  sound.value?.off().stop();
+  sound.value = undefined;
+};
+
+const playMusic = (music?: (typeof musics)[number]) => {
+  if (!music) music = sample(musics);
+  currentMusic.value = music!;
+  if (sound.value) stopMusic();
+  sound.value = new Howl({
+    src: [music!.src],
+    html5: true,
+    autoplay: true,
+    onend: () => playMusic(),
+    onloaderror: () => stopMusic(),
+    onplayerror: () => stopMusic(),
+  });
+};
+
+const musicOptions = computed(() => {
+  const options = musics.map((music) => {
+    const item: DropdownItem = {
+      label: music.label,
+      click: () => playMusic(music),
+      icon: music.src === currentMusic.value.src ? "i-tabler-music" : undefined,
+    };
+    return item;
+  });
+  return [options];
+});
+
+const handleClickPlayMusic = () => {
+  if (sound.value) stopMusic();
+  else playMusic();
+};
+
+const initPlayMusic = once(() => {
+  playMusic();
+});
+
+useEventListener("mousedown", initPlayMusic, { once: true });
+useEventListener("touchmove", initPlayMusic, { once: true });
+
+onUnmounted(() => {
+  stopMusic();
+});
 </script>
 
 <template>
@@ -8,6 +70,16 @@ const navbar = defineModel<boolean>("navbar");
     :class="$style.header"
   >
     <h1 class="flex-1">大道之行也 天下为公</h1>
+    <UDropdown :items="musicOptions" mode="hover">
+      <UButton
+        square
+        icon="i-tabler-brand-netease-music"
+        style="border-radius: 50%"
+        :class="{ 'animate-pulse': sound }"
+        :color="sound ? 'violet' : 'primary'"
+        @click="handleClickPlayMusic"
+      />
+    </UDropdown>
     <UToggle
       v-model="navbar"
       on-icon="i-tabler-menu"
