ee7ea39b6c401204c687fd19385ca4a990d46c42
WangZhenghai <fisschl@163.com>
2024-03-27 22:24:58 +0800

    feat: 移除

--- components/AdvancedTable/AdvancedTable.vue
+++ components/AdvancedTable/AdvancedTable.vue
@@ -3,11 +3,6 @@ import { computed, toRef } from "vue";
 import { useVirtualizer } from "@tanstack/vue-virtual";
 import { Table } from "./schema";
 import type { UnRef } from "./utils";
-import TableRow from "./TableRow.vue";
-import table_style from "./table.module.css";
-import HeaderCell from "./HeaderCell.vue";
-import WidthHandler from "./WidthHandler.vue";
-import BoxSelector from "./BoxSelector.vue";
 
 const props = defineProps<{
   table: Table;
@@ -55,69 +50,28 @@ const virtual_rows = computed(() => rows_virtual.value.getVirtualItems());
 const virtual_columns = computed(() => columns_virtual.value.getVirtualItems());
 
 const columns = toRef(table, "columns");
+
+const { width, height } = useElementSize(container);
 </script>
 
 <template>
   <div ref="container" class="relative overflow-auto" :class="$style.container">
-    <table
-      class="relative block"
+    <div
+      class="sticky left-0 top-0"
+      :style="{
+        width: `${width}px`,
+        height: `${height}px`,
+      }"
+    >
+      天下为公
+    </div>
+    <p
+      class="m-0 p-0"
       :style="{
         width: `${total_width}px`,
         height: `${total_height}px`,
       }"
-    >
-      <thead class="sticky top-0 z-10 block">
-        <tr
-          class="flex"
-          :style="{
-            width: `${total_width}px`,
-            height: `${table.ITEM_HEIGHT}px`,
-          }"
-        >
-          <th
-            class="sticky left-0 z-10 justify-center bg-[--header-background]"
-            :style="{
-              width: `${table.ITEM_HEIGHT}px`,
-              height: `${table.ITEM_HEIGHT}px`,
-            }"
-            :class="table_style.cell"
-          >
-            <input type="checkbox" />
-          </th>
-          <HeaderCell
-            v-for="column in virtual_columns"
-            :key="columns[column.index].name"
-            :style="{
-              width: `${column.size}px`,
-              height: `${table.ITEM_HEIGHT}px`,
-              transform: `translateX(${column.start}px)`,
-            }"
-            :table="table"
-            :virtual-column="column"
-          />
-        </tr>
-      </thead>
-      <tbody class="block">
-        <TableRow
-          v-for="row in virtual_rows"
-          :key="row.key"
-          :table="table"
-          :style="{
-            width: `${total_width}px`,
-          }"
-          :virtual-row="row"
-          :virtual-columns="virtual_columns"
-        />
-      </tbody>
-    </table>
-    <WidthHandler
-      :table="table"
-      :style="{
-        height: `${total_height}px`,
-      }"
-      :virtualizer="columns_virtual"
-    />
-    <BoxSelector :table="table" />
+    ></p>
   </div>
 </template>
 
