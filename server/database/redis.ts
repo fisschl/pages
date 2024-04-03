673901218e22cc886c7fa285a7f1db24c59e5420
WangZhenghai <fisschl@163.com>
2024-04-03 15:08:42 +0800

    feat: redis 连接

--- server/database/redis.ts
+++ server/database/redis.ts
@@ -1,10 +1,17 @@
 import Redis from "ioredis";
-import { Processor, Queue, Worker } from "bullmq";
+import type { Processor } from "bullmq";
+import { Queue, Worker } from "bullmq";
+import { URL } from "node:url";
 
-const { REDIS_URL } = process.env;
-
-export const redis = new Redis(REDIS_URL!);
-redis.options.maxRetriesPerRequest = null;
+const uri = new URL(process.env.REDIS_URL!);
+export const redis = new Redis({
+  username: uri.username,
+  password: uri.password,
+  host: uri.hostname,
+  port: parseInt(uri.port),
+  db: 0,
+  maxRetriesPerRequest: null,
+});
 
 export const HOUR = 60 * 60;
 export const DAY = 24 * HOUR;
