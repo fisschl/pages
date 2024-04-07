4392c97eb9546e3cac9220ac457ce6367cd35bd7
WangZhenghai <fisschl@163.com>
2024-04-07 14:13:18 +0800

    feat: 调整部分样式

--- components/chat/Message.vue
+++ components/chat/Message.vue
@@ -42,13 +42,13 @@ const handleCommand = async (command: string) => {
     <section
       class="relative cursor-auto rounded px-3 py-2"
       :class="{
-        'bg-stone-500/10': message.role === 'assistant',
-        'bg-violet-500/15': message.role === 'user',
+        'bg-stone-400/10 dark:bg-stone-500/10': message.role === 'assistant',
+        'bg-violet-500/10 dark:bg-violet-500/20': message.role === 'user',
       }"
       :data-id="message.id"
     >
       <article
-        class="prose prose-sm max-w-none dark:prose-invert"
+        class="prose prose-sm max-w-none dark:prose-invert prose-code:text-sm"
         v-html="message.content"
       />
       <img
