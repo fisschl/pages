59d7432773f4250fa717210f1f57bbb6389c7f61
WangZhenghai <fisschl@163.com>
2024-06-06 14:16:11 +0800

    移除基础镜像

--- infrastructure/docker/node.ps1
+++
@@ -1,5 +0,0 @@
-$image = "registry.cn-shanghai.aliyuncs.com/fisschl/node:latest"
-
-docker pull node:lts
-docker tag node:lts $image
-docker push $image
--- infrastructure/docker/pnpm/build.ps1
+++
@@ -1,4 +0,0 @@
-$image = "registry.cn-shanghai.aliyuncs.com/fisschl/pnpm:latest"
-
-docker build -t $image .
-docker push $image
