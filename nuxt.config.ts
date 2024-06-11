686c67793b6d442b541983bcbb0445d34a9c0295
WangZhenghai <fisschl@163.com>
2024-06-11 10:11:19 +0800

    新增日志存储

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,10 @@
-import "dotenv/config";
-
-const development = process.env.NODE_ENV === "development";
-
 export default defineNuxtConfig({
   modules: [
-    development ? "@nuxt/devtools" : false,
+    "@nuxt/devtools",
     "@nuxt/ui",
     "@vueuse/nuxt",
+    "@nuxt/eslint",
     "@pinia/nuxt",
-    development ? "@nuxt/eslint" : false,
   ],
 
   ui: {
--- server/api/translate/index.ts
+++ server/api/translate/index.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 import { parseMarkdown } from "~/server/api/markdown";
 import { publisher } from "~/server/database/mqtt";
 import { useToken } from "~/server/utils/user";
+import { writeLog } from "~/server/database/clickhouse";
 
 const zh_prompt = `
 你是一个专业翻译引擎，你擅长将任何语言翻译为中文。你会完整，确切地翻译我的话，尽量保证信达雅。仅需给出翻译，无需解释。
@@ -20,35 +21,45 @@ const request_schema = z.object({
 export default defineEventHandler(async (event) => {
   const body = await readValidatedBody(event, request_schema.parse);
   body.content = body.content.replace(/\n+/g, "\n\n");
-  const stream = await openai.chat.completions.create({
-    model: "moonshot-v1-8k",
-    messages: [
-      {
-        role: "system",
-        content: zh_prompt,
-      },
-      {
-        role: "user",
-        content: body.content,
-      },
-    ],
-    stream: true,
-    max_tokens: 2048,
-  });
-  const output = {
-    content: "",
-  };
-  const token = useToken(event);
-  for await (const { choices } of stream) {
-    if (!choices.length) continue;
-    const [{ delta }] = choices;
-    if (!delta || !delta.content) continue;
-    output.content += delta.content;
-    const message = {
-      ...output,
-      content: await parseMarkdown(output.content),
+  try {
+    await writeLog("请求翻译", body.content);
+    const stream = await openai.chat.completions.create({
+      model: "moonshot-v1-8k",
+      messages: [
+        {
+          role: "system",
+          content: zh_prompt,
+        },
+        {
+          role: "user",
+          content: body.content,
+        },
+      ],
+      stream: true,
+      max_tokens: 2048,
+    });
+    const output = {
+      content: "",
     };
-    publisher.publish(`public/translate/${token}`, JSON.stringify(message));
+    const token = useToken(event);
+    for await (const { choices } of stream) {
+      if (!choices.length) continue;
+      const [{ delta }] = choices;
+      if (!delta || !delta.content) continue;
+      output.content += delta.content;
+      const message = {
+        ...output,
+        content: await parseMarkdown(output.content),
+      };
+      publisher.publish(`public/translate/${token}`, JSON.stringify(message));
+    }
+    return output;
+  } catch (e) {
+    const content = JSON.stringify({
+      error: e,
+      content: body.content,
+    });
+    await writeLog("请求翻译失败", content);
+    throw createError({ status: 500 });
   }
-  return output;
 });
