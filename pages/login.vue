0f6563b04520b9256f7866c84181dcd31b65ccb0
WangZhenghai <fisschl@163.com>
2024-06-14 09:11:48 +0800

    登陆提示

--- pages/login.vue
+++ pages/login.vue
@@ -10,7 +10,9 @@ const cookie = useCookie("token");
 /**
  * https://gitee.com/api/v5/oauth_doc
  */
-const login = async (from: string) => {
+const login = async () => {
+  const { from } = query;
+  if (!from || !isString(from)) return;
   if (cookie.value && store.user) {
     // 有登录态，直接返回
     const uri = new URL(from);
@@ -23,24 +25,29 @@ const login = async (from: string) => {
   }
 };
 
-const access = async (code: string) => {
+onMounted(async () => {
+  const { code } = query;
+  if (!code || !isString(code)) return;
   const res = await $fetch("/api/auth/access", {
     query: { code },
   });
   store.user = res;
   const from = localStorage.getItem("pages_login_from");
+  localStorage.removeItem("pages_login_from");
   if (!from) location.href = location.origin;
   else location.href = from;
-};
-
-onMounted(async () => {
-  const { from } = query;
-  if (from && isString(from)) return login(from);
-  const { code } = query;
-  if (code && isString(code)) return access(code);
 });
 </script>
 
 <template>
-  <UContainer class="h-dvh"> </UContainer>
+  <UContainer class="flex h-dvh items-center justify-center py-6">
+    <UCard>
+      <p class="mb-6 flex items-center justify-center text-base font-semibold">
+        请登录后使用
+      </p>
+      <UButton icon="i-tabler-brand-git" @click="login">
+        使用 Gitee 登录
+      </UButton>
+    </UCard>
+  </UContainer>
 </template>
