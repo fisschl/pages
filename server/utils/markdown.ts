26bf6a2f7c781f38f28a2a7004370b5d637dae46
WangZhenghai <fisschl@163.com>
2024-07-01 18:07:38 +0800

    use redis cache

--- server/utils/markdown.ts
+++ server/utils/markdown.ts
@@ -1,8 +1,8 @@
 import Shiki from "@shikijs/markdown-it";
 import { once } from "lodash-es";
 import MarkdownIt from "markdown-it";
-import { LRUCache } from "lru-cache";
 import { xxhash3 } from "hash-wasm";
+import { DAY, redis } from "../database/redis";
 
 const load_markdown = once(async () => {
   const markdown = MarkdownIt();
@@ -16,10 +16,6 @@ const load_markdown = once(async () => {
   return markdown.use(shiki);
 });
 
-export const markdown_cache = new LRUCache<string, string>({
-  max: 1024,
-});
-
 export const parseMarkdown = async (text: string) => {
   const markdown = await load_markdown();
   return markdown.render(text);
@@ -27,10 +23,10 @@ export const parseMarkdown = async (text: string) => {
 
 export const parseMarkdownCache = async (text: string) => {
   const hash = await xxhash3(text);
-  const cache_result = markdown_cache.get(hash);
+  const cache_result = await redis.get(hash);
   if (cache_result) return cache_result;
   const markdown = await load_markdown();
   const result = markdown.render(text);
-  markdown_cache.set(hash, result);
+  await redis.setex(hash, 30 * DAY, result);
   return result;
 };
