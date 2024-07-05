0da7549a1b20af91b3ed98c930864d11950c8bde
WangZhenghai <fisschl@163.com>
2024-07-05 16:12:45 +0800

    使用 Form 预设

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,10 @@
 import type { Config } from "tailwindcss";
 import typography from "@tailwindcss/typography";
 import ratio from "@tailwindcss/aspect-ratio";
+import forms from "@tailwindcss/forms";
 
 const config: Partial<Config> = {
-  plugins: [typography, ratio],
+  plugins: [typography, ratio, forms],
 };
 
 export default config;
