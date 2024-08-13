f8a6227358aa9d57814ed3a623a459ee5ebcb1eb
WangZhenghai <fisschl@163.com>
2024-08-13 16:10:27 +0800

    更改为月之暗面

--- server/api/moonshot.ts
+++ server/api/moonshot.ts
@@ -1,23 +1,24 @@
 import OpenAI from "openai";
 
 const openai = new OpenAI({
-  apiKey: process.env["DASHSCOPE_API_KEY"],
-  baseURL: "https://dashscope.aliyuncs.com/compatible-mode/v1",
+  apiKey: process.env["MOONSHOT_API_KEY"],
+  baseURL: "https://api.moonshot.cn/v1",
 });
+
+export const runTask = (func: () => unknown) => func();
 
 export default defineEventHandler(async (event) => {
   const { model, messages } = await readBody(event);
   const stream = await openai.chat.completions.create({
-    model: model || "qwen-max",
+    model: model || "moonshot-v1-32k",
     messages,
     stream: true,
   });
   const eventStream = createEventStream(event);
-  setTimeout(async () => {
-    for await (const item of stream) {
+  runTask(async () => {
+    for await (const item of stream)
       await eventStream.push(JSON.stringify(item));
-    }
     await eventStream.close();
-  }, 60);
+  });
   await eventStream.send();
 });
