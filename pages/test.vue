59636d3bd3c05528d1e639008db25d7aa29956d1
WangZhenghai <fisschl@163.com>
2024-08-16 15:03:48 +0800

    测试页面

--- pages/test.vue
+++ pages/test.vue
@@ -1,9 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const getPlusStorage = (key: string): string => {
+  // @ts-expect-error 在 web-view 加载的 HTML 中调用 uni 的 API，需要在 HTML 中引用必要的 JS-SDK
+  return plus.storage.getItem(key);
+};
+
+const token = ref("");
+
+onMounted(async () => {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  token.value = getPlusStorage("LOGIN_TOKEN");
+});
+</script>
 
 <template>
-  <UContainer class="my-6">
-    <UButton @click="() => {}">我是山里灵活的狗</UButton>
-  </UContainer>
+  <UContainer class="my-6"> LOGIN_TOKEN： {{ token }} </UContainer>
 </template>
 
 <style module></style>
