b786b3d13057705712735ed33fb6938a76204ffe
WangZhenghai <fisschl@163.com>
2024-12-05 19:41:56 +0800

    修改图片查看器

--- components/ImageViewer.vue
+++ components/ImageViewer.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
 import { create, zoom } from "d3";
 
-const teleportContainer = useTemplateRef<HTMLElement>("teleport-container");
+const props = defineProps<{
+  src?: string | null;
+}>();
+
+const container = useTemplateRef<HTMLElement>("image-container");
 const isViewerVisible = ref(false);
 
 const openImageViewer = async (url: string) => {
@@ -13,7 +17,7 @@ const openImageViewer = async (url: string) => {
   });
   await nextTick();
   const svg = create("svg").style("width", "100%").style("height", "100%");
-  teleportContainer.value?.append(svg.node()!);
+  container.value!.append(svg.node()!);
   const g = svg.append("g");
   const zoomBehavior = zoom<any, any>()
     .scaleExtent([0.5, 2])
@@ -21,7 +25,7 @@ const openImageViewer = async (url: string) => {
       g.attr("transform", transform);
     });
   svg.call(zoomBehavior);
-  const { width, height } = teleportContainer.value!.getBoundingClientRect();
+  const { width, height } = container.value!.getBoundingClientRect();
   const { naturalWidth, naturalHeight } = image;
   const scale = Math.min(width / naturalWidth, height / naturalHeight) * 0.9;
   const left = (width - naturalWidth * scale) / 2;
@@ -34,32 +38,24 @@ const openImageViewer = async (url: string) => {
     .attr("y", top);
 };
 
-useEventListener("click", (e) => {
-  if (e.target === teleportContainer.value) {
-    isViewerVisible.value = false;
-    return;
-  }
-  const { target } = e;
-  if (!(target instanceof Element)) return;
-  const img = target.closest("img");
-  if (img?.closest(".image-viewer-target")) {
-    openImageViewer(img.src);
-    return;
-  }
+whenever(toRef(props, "src"), (src) => {
+  openImageViewer(src);
 });
 </script>
 
 <template>
-  <Teleport v-if="isViewerVisible" to="body">
+  <Teleport to="body">
     <div
-      ref="teleport-container"
-      class="teleport-container bg-gray-500/40 backdrop-blur-lg"
-    />
+      v-if="isViewerVisible"
+      class="image-viewer-container bg-gray-500/40 backdrop-blur-lg"
+    >
+      <article ref="image-container" class="absolute h-full w-full"></article>
+    </div>
   </Teleport>
 </template>
 
 <style scoped>
-.teleport-container {
+.image-viewer-container {
   position: fixed;
   left: 0;
   top: 0;
