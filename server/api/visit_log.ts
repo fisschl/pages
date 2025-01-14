23e2ae65099c3ae8ca96e9e759484046e86a28f7
WangZhenghai <fisschl@163.com>
2025-01-14 16:31:40 +0800

    优化访问日志记录功能，增加数据验证和节流机制

--- server/api/visit_log.ts
+++ server/api/visit_log.ts
@@ -1,9 +1,26 @@
 import { database } from "~/server/database/postgres";
+import { throttle } from "lodash-es";
+import { z } from "zod";
+
+const InsertSchema = z.object({
+  full_path: z.string(),
+  ua: z.string(),
+});
+
+type InsertBody = z.infer<typeof InsertSchema>;
+
+const data: InsertBody[] = [];
+
+const insert = throttle(async () => {
+  const list = data.splice(0, data.length);
+  await database.visit_logs.createMany({
+    data: list,
+  });
+}, 3 * 1000);
 
 export default defineEventHandler(async (event) => {
-  const body = await readBody(event);
-  await database.visit_logs.create({
-    data: body,
-  });
+  const body = await readValidatedBody(event, InsertSchema.parse);
+  data.push(body);
+  insert();
   return { message: "ok" };
 });
