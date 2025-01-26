3c3020db06b8772a4e93b38b23fb3cd1975855c8
WangZhenghai <fisschl@163.com>
2025-01-26 13:42:01 +0800

    重构批量插入工具并新增重定向页面

---
+++ pages/translate.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+onMounted(() => {
+  location.href = "https://bronya.world/translate/articles";
+});
+</script>
+
+<template>
+  <div class="flex h-dvh items-center justify-center">
+    <p class="text-lg">Redirecting...</p>
+  </div>
+</template>
--- server/api/visit_log.ts
+++ server/api/visit_log.ts
@@ -1,18 +1,17 @@
 import { database } from "~/server/database/postgres";
 import { z } from "zod";
-import pLimit from "p-limit";
+import { throttle } from "lodash-es";
 
 export const defineBatchInsert = <T>(handler: (items: T[]) => unknown) => {
   const data: T[] = [];
-  const limit = pLimit(1);
+  const insert = throttle(async () => {
+    if (!data.length) return;
+    const list = data.splice(0, data.length);
+    await handler(list);
+  }, 3 * 1000);
   return (item: T) => {
     data.push(item);
-    if (limit.pendingCount > 1) return;
-    limit(async () => {
-      if (!data.length) return;
-      const list = data.splice(0, data.length);
-      await handler(list);
-    });
+    insert();
   };
 };
 
